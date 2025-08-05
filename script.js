b996f68719e745817c5575b46b93efea761b2b16
Muntasirjadid18 <jadid.muntasir2002@gmail.com>
2025-08-06 02:06:36 +0600

    withdraw deposit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+document.getElementById("depositBtn").addEventListener("click",function()
+{
+const currentTotal = parseFloat( document.getElementById("totalAmount").innerText);
+const depositAmount = parseFloat( document.getElementById("depositInp").value);
+const newTotal = currentTotal+depositAmount;
+document.getElementById("totalAmount").innerText = newTotal;
+document.getElementById("depositInp").value = "";
+})
+document.getElementById("withdrawBtn").addEventListener("click",function(){
+   const newTotal = parseFloat(document.getElementById("totalAmount").innerText);
+   const withdrawAmount = parseFloat(document.getElementById("withdrawInp").value);
+   const amount = newTotal - withdrawAmount;
+   document.getElementById("totalAmount").innerText = amount;
+   document.getElementById("withdrawInp").value ="";
+})
