5b9808cfdda477256ff130c195c7ba212505dc28
Muntasirjadid18 <jadid.muntasir2002@gmail.com>
2025-08-06 06:34:31 +0600

    javascript

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ document.getElementById("depositInp").value = "";
 document.getElementById("withdrawBtn").addEventListener("click",function(){
    const newTotal = parseFloat(document.getElementById("totalAmount").innerText);
    const withdrawAmount = parseFloat(document.getElementById("withdrawInp").value);
+   if(withdrawAmount > newTotal){
+    alert('insufficient balance');
+    document.getElementById("withdrawInp").value ="";
+    return;
+      
+   }
    const amount = newTotal - withdrawAmount;
    document.getElementById("totalAmount").innerText = amount;
    document.getElementById("withdrawInp").value ="";
